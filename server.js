143136302ca0de615a20859326ed80d4482b2298
Roni <roniardiansyah0@gmail.com>
2026-07-20 04:12:33 +0700

    Initial Saweria Server setup

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+// Halaman utama
+app.get("/", (req, res) => {
+    res.send("Saweria Server Berjalan!");
+});
+
+// Webhook Saweria
+app.post("/webhook", (req, res) => {
+
+    console.log("========== DONASI MASUK ==========");
+    console.log(req.body);
+    console.log("=================================");
+
+    res.status(200).send("OK");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di port ${PORT}`);
+});
